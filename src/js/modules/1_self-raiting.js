3a3d2af7000a636af53d48fea38691c114a265ac
Eugene <eugvok@gmail.com>
2024-11-23 18:04:06 +0300

    init: start project2 task

---
+++ src/js/modules/1_self-raiting.js
@@ -0,0 +1,1 @@
+console.log("score\n \n \n ...");
---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+"use strict"
+
+// import { } from "./modules/self-raiting.js";
+// import { } from "./modules/mesage.js";
+// import { } from "./modules/burger.js";
+// import { } from "./modules/slider.js";
+// import { } from "./modules/popup.js";
